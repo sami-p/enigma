71061447df2a483fcd7ee8ca69cc95464a1036e0
sami-p <sami.peterson14@gmail.com>
2021-06-12 17:49:48 -0500

    Add before :all

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,23 +1,22 @@
 require 'spec_helper'
 
 RSpec.describe Enigma do
+  before :all do
+    @enigma = Enigma.new
+  end
   describe '#initialize' do
     it 'exists' do
-      enigma = Enigma.new
-
-      expect(enigma).to be_an_instance_of(Enigma)
+      expect(@enigma).to be_an_instance_of(Enigma)
     end
 
     it 'encrypts a message' do
-      enigma = Enigma.new
-
       text = ({
                 encryption: "keder ohulw",
                 key: "02715",
                 date: "040895"
                 })
 
-      expect(enigma.encrypt("hello world", "02715", "040895")).to eq(text)
+      expect(@enigma.encrypt("hello world", "02715", "040895")).to eq(text)
     end
   end
 end
