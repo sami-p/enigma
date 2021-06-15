c26c706bb32eefee7c5ad00c7f3b260ec06a5ee5
sami-p <sami.peterson14@gmail.com>
2021-06-15 08:06:04 -0500

    Align spacing

--- spec/key_spec.rb
+++ spec/key_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Key do
   describe 'generate' do
     it 'can generate a random 5 digit key' do
       key = Key.generate
+      
       expect(key).to be_a(String)
       expect(key.length).to eq(5)
     end
